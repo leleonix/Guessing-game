482a0b1ca1578d56704f02b1de6a08fc471b53ab
leleonix <162342352+leleonix@users.noreply.github.com>
2024-06-03 15:58:25 +0200

    database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://game_user:game_password@localhost/guessing_game_db"
+DATABASE_URL = "postgresql://game_user:password12+@localhost/guessing_game_db"
 
 engine = create_engine(
     DATABASE_URL
