6262998855bc2cf117ee58d34977e5d527662128
leleonix <162342352+leleonix@users.noreply.github.com>
2024-06-03 16:56:44 +0200

    database.py

--- database.py
+++ database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "postgresql://game_user:password12+@localhost/guessing_game_db"
 
+if os.getenv("CONNECTION_STRING"):
+    SQLALCHEMY_DATABASE_URL = os.getenv("CONNECTION_STRING")
+else:
+    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@host.docker.internal:5432/fastapidb"
+
 engine = create_engine(
     DATABASE_URL
 )
